27ebd33438971b078d71cd541d1bcc96b71997ea
Jorge Baez <jibm_82@yahoo.com.mx>
2018-11-17 02:44:59 -0600

    Updated server

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 8080;
 const apiRoutes = require("./app/routing/apiRoutes");
 const htmlRoutes = require("./app/routing/htmlRoutes");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('app/public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
